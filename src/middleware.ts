6be6ee0ce635dc37337485884e523ee5e0776c16
Markus Westphal <markuswestphal@MacBook-Pro-von-Markus.local>
2024-08-13 18:04:39 +0200

    require a certain password (although identical for all users to keep it simple)

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,16 @@
 import {NextRequest, NextResponse} from "next/server";
 import {extractCredentials} from "@/lib/auth/auth.service";
 
+const password = process.env['AUTH_PASSWORD'] ?? 'secret';
+
 export function middleware(req: NextRequest) {
     const credentials = extractCredentials(req.headers);
     if (credentials === 'notFound') {
+        return new NextResponse('Authentication required', {
+            status: 401,
+            headers: { 'WWW-Authenticate': 'Basic' },
+        });
+    } else if (credentials.pass !== password) {
         return new NextResponse('Authentication required', {
             status: 401,
             headers: { 'WWW-Authenticate': 'Basic' },
